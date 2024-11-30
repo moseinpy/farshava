d78a15da73b97b5ce1cbf6689eb4149c8caed224
mosein <mosein.56@gmail.com>
2024-11-30 18:40:52 +0330

    rainfall-24h 30-11-2024

--- weather_watch_project/settings.py
+++ weather_watch_project/settings.py
@@ -133,6 +133,17 @@ LOGIN_URL = '/login/'
 #        'PORT': '5432',
 #    }
 # }
+#
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "root",
+        "PASSWORD": "MkJxFnccgV1zp2pdixmw4ncx",
+        "HOST": "postgres-db",
+        "PORT": "5432",
+    }
+}
 
 DATABASES = {
     "default": {
